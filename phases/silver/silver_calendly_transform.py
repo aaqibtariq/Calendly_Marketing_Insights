48d07b501e7cf0cbea3016ae8300ff55baab2417
aaqibtariq <38801027+aaqibtariq@users.noreply.github.com>
2026-05-27 12:52:37 -0400

    Update silver_calendly_transform.py

--- phases/silver/silver_calendly_transform.py
+++ phases/silver/silver_calendly_transform.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, lit, to_date, hour, dayofweek, when
+from pyspark.sql.functions import col, to_date, hour, dayofweek, when
 
 spark = (
     SparkSession.builder
@@ -34,7 +34,7 @@ silver_df = (
         col("raw_payload.payload.tracking.utm_source").alias("utm_source"),
         col("raw_payload.payload.tracking.utm_campaign").alias("utm_campaign"),
         col("ingestion_timestamp"),
-        col("ingestion_date")
+        to_date(col("ingestion_date")).alias("ingestion_date")
     )
 )
 
@@ -42,18 +42,27 @@ silver_df = (
     silver_df
     .withColumn(
         "channel",
-        when(col("event_type_uri") == "https://api.calendly.com/event_types/d639ecd3-8718-4068-955a-436b10d72c78", "facebook_paid_ads")
-        .when(col("event_type_uri") == "https://api.calendly.com/event_types/dbb4ec50-38cd-4bcd-bbff-efb7b5a6f098", "youtube_paid_ads")
-        .when(col("event_type_uri") == "https://api.calendly.com/event_types/bb339e98-7a67-4af2-b584-8dbf95564312", "tiktok_paid_ads")
+        when(
+            col("event_type_uri") == "https://api.calendly.com/event_types/d639ecd3-8718-4068-955a-436b10d72c78",
+            "facebook_paid_ads"
+        )
+        .when(
+            col("event_type_uri") == "https://api.calendly.com/event_types/dbb4ec50-38cd-4bcd-bbff-efb7b5a6f098",
+            "youtube_paid_ads"
+        )
+        .when(
+            col("event_type_uri") == "https://api.calendly.com/event_types/bb339e98-7a67-4af2-b584-8dbf95564312",
+            "tiktok_paid_ads"
+        )
         .otherwise("unknown")
     )
     .withColumn("booking_date", to_date(col("booking_created_at")))
     .withColumn("meeting_date", to_date(col("meeting_start_time")))
     .withColumn("meeting_hour", hour(col("meeting_start_time")))
     .withColumn("meeting_day_of_week", dayofweek(col("meeting_start_time")))
+    .filter(col("channel") != "unknown")
+    .dropDuplicates(["event_id"])
 )
-
-silver_df = silver_df.filter(col("channel") != "unknown")
 
 silver_df.write.format("delta").mode("overwrite").save(silver_path)
 
